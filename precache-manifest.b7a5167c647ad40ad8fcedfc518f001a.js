4cab2b948e041a0baa44034fe8d1fe690d057418
calebclayson <clayson.caleb@gmail.com>
2019-04-16 09:17:29 -0600

    Updates

---
+++ precache-manifest.b7a5167c647ad40ad8fcedfc518f001a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d774eea02c5ef898362",
+    "url": "/ReactECommerce/static/js/runtime~main.d8e7db8d.js"
+  },
+  {
+    "revision": "2bb057f5049f4b165407",
+    "url": "/ReactECommerce/static/js/main.b6eee01f.chunk.js"
+  },
+  {
+    "revision": "4dd2493827f5e55cfd09",
+    "url": "/ReactECommerce/static/js/2.c8be6743.chunk.js"
+  },
+  {
+    "revision": "2bb057f5049f4b165407",
+    "url": "/ReactECommerce/static/css/main.8905c0ac.chunk.css"
+  },
+  {
+    "revision": "de525812754fc7acb8d313d26dd73e63",
+    "url": "/ReactECommerce/index.html"
+  }
+];
